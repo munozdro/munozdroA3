c6a5fa6cbf3f607fd7c7691531eb6160f38dc8b9
munozdro <52796574+munozdro@users.noreply.github.com>
2019-07-26 22:53:04 -0400

    upload of files
    Jose Miguel Munoz Droguett

---
+++ github/assets/data/myBooks.ts
@@ -0,0 +1,9 @@
+import {Books} from '../../app/books';
+
+export const BOOKS_LIST: Books[]=
+[
+    {book_name:'Vogage extraordinaires', author: 'Jules Verne', book_genre:'Fiction', year_published: '1864', picture:'voyages_extraordinaires.jpg'},
+    {book_name:'Le petit prince', author: 'Antoine de Saint-exupery', book_genre:'Encyclopedic novels', year_published: '1944', picture:'le_petit_prince.jpg'},
+    {book_name:'The Da Vinci Code', author: 'Dan Brown', book_genre:'Mystery', year_published: '2003', picture:'da_vinci_code.jpg'},
+    {book_name:'The Art of War', author: 'Sun Tzu', book_genre:'Chinese military treatise', year_published: '500 A.C', picture:'art_war.jpg'}
+];
